1961ef80ea87ea2170bd9cf01c67f9f141dd4777
David Stroud <david@dwstroud.com>
2016-02-03 17:49:25 +0000

    Added button sound and simple label update function

--- space-calc/ViewController.swift
+++ space-calc/ViewController.swift
@@ -7,14 +7,70 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Add = "+"
+        case Subtract = "-"
+        case Equals = "="
+        case Empty = "Empty"
+    }
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var currentOperation: Operation = Operation.Empty
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        
+        // we have to put this bit in a DO statement, and catch any error (even if we're sure there won't be an error!) otherwise swift complains.
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundUrl)
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
     }
-
+    
+    // we've tagged each button with a tag corresponding to the number
+    // so we only need to write one function for numbers being pressed
+    @IBAction func numberPressed(btn: UIButton!) {
+        btnSound.play()
+        runningNumber += "\(btn.tag)"
+        outputLbl.text = runningNumber
+    }
+    
+    // set up our green button actions!
+    @IBAction func onDividePressed(sender: AnyObject) {
+        
+    }
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        
+    }
+    @IBAction func onAddPressed(sender: AnyObject) {
+        
+    }
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        
+    }
+    @IBAction func onEqualsPressed(sender: AnyObject) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
